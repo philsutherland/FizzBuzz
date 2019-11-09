6f86918bc154f0c6a15b18566c1a04b951da5294
Phil Sutherland <psuth026@uottawa.ca>
2019-11-08 19:11:37 -0500

    Example code

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello World")
+def fizz_buzz(input):
+    if(input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    elif input % 3 == 0:
+        return "Fizz"
+    elif input % 5 == 0:
+        return "Buzz"
+    else:
+        return input
+
+
+print(fizz_buzz(int(input("Enter an integer: "))))
